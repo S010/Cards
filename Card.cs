9c08c83344683e39755c62c6d8b014ef3cf374f6
Jacob Morgan <jake2002.jm@gmail.com>
2021-04-06 20:06:37 +0100

    Dot net init

--- Card.cs
+++ Card.cs
@@ -2,10 +2,8 @@ namespace cards
 {
     public class Card
     {
-
-       // This object handles individual cards
-        
-        public string Suit {get; set;}
+        public string Suit { get; set; }
         public string Value { get; set; }
+        
     }
 }
