59e36bd79b7704fba701c5ff05a63e8fe8b9344c
Jacob Morgan <jake2002.jm@gmail.com>
2021-04-06 16:24:01 +0100

    code review

---
+++ Card.cs
@@ -0,0 +1,11 @@
+namespace cards
+{
+    public class Card
+    {
+
+       // This object handles individual cards
+        
+        public string Suit {get; set;}
+        public string Value { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            while(true)
+            {
+                Console.WriteLine("Card Dealer!\nENTER to create a new deck\nQ to Quit");
+                switch (Console.ReadLine().ToLower())
+                {
+                    case "":
+                        deck.newDeck();
+                        deck.shuffle();
+                        deck.dealerMenu();
+                        break;
+                    case "q":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
