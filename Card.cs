82e5239c889797435385573932f2f17b0e3b2fa2
Jacob Morgan <jake2002.jm@gmail.com>
2021-04-07 16:27:30 +0100

    Added Vital Features

--- Card.cs
+++ Card.cs
@@ -2,6 +2,7 @@ namespace cards
 {
     public class Card
     {
+        // This object handles individual cards
         public string Suit { get; set; }
         public string Value { get; set; }
         
